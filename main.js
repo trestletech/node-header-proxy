f22ed4ca530cbda558d2431f61c87d523509b89a
trestletech <jeff.allen@trestletechnology.net>
2013-10-25 15:29:11 -0500

    Added basic HTTP and WS proxy which sets auth header for WS.

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// We'll do some things here!
+var _ = require('underscore');
+
+var http = require('http');
+var httpProxy = require('http-proxy');
+
+
+var port = 8000;
+var destPort = 3838;
+var destHostname = 'localhost';
+
+var server = httpProxy.createServer(function (req, res, proxy) {
+
+  proxy.proxyRequest(req, res, {
+    host: destHostname,
+    port: destPort
+  });
+});
+
+server.listen(port);
+server.on('upgrade', function (req, socket, head) {
+
+    req.headers['SHINY_SERVER_CREDENTIALS'] = '{"user": "jeff", "groups":["group1", "group2"]}';
+
+    server.proxy.proxyWebSocketRequest(req, socket, head, {host: destHostname, port: destPort});
+});
