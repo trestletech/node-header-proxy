35e4c7b149a3e568919d13d8e54f66aa000f434d
trestletech <jeff.allen@trestletechnology.net>
2013-11-07 10:05:53 -0600

    Add headers only on HTTP requests, but now support HTTPS.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var _ = require('underscore');
 var http = require('http');
 var httpProxy = require('http-proxy');
+var https = require('https');
+var fs = require('fs');
 
 var opts = require('optimist')
-  .usage('Test auth proxy.\nUsage: $0 [extPort] [destPort] [destHostName]')
+  .usage('Test auth proxy.\nUsage: $0 [extPort] [destPort] [destHostName] --ssl')
+  .describe('ssl', 'Enable SSL on both the client and expect it on the target.')
   .demand(0);
 var argv = opts.argv;
 
@@ -11,33 +14,36 @@ var port = argv._[0]?argv._[0] : 8000;
 var destPort = argv._[1]?argv._[1] : 3838;
 var destHostname = (argv._[2])?argv._[2] : 'localhost';
 
-var server = httpProxy.createServer(function (req, res, proxy) {
-  /**
-   * Add headers onto HTTP requests. Shiny Server Pro will check the whitelist_headers
-   * property to see if it should save and then forward this header onto the Shiny
-   * web socket connection. If so, this object should be available as the
-   * session$request object.
-   */
-  req.headers['SOME_AUTH'] = 'SOMETHING';
-  
-  proxy.proxyRequest(req, res, {
-    host: destHostname,
-    port: destPort
-  });
+var options = {
+  target: {
+    https: argv.ssl,
+    rejectUnauthorized: false
+  }
+}
+
+if (argv.ssl){
+  options.https = {
+    key: fs.readFileSync('/home/jeff/workspace/test-key.pem'),
+    cert: fs.readFileSync('/home/jeff/workspace/test-cert.pem')
+  }
+}
+var proxy = new httpProxy.HttpProxy({ 
+  target: {
+    host: destHostname, 
+    port: destPort,
+    https: argv.ssl,
+    rejectUnauthorized: false
+  }
 });
 
-server.listen(port);
-server.on('upgrade', function (req, socket, head) {
-    /**
-     * This line will add a header onto the /web socket/ request. This is how Shiny gets
-     * information. In particular, the SHINY_SERVER_CREDENTIALS header is a special header
-     * which will be parsed as JSON then added to the Shiny session as 'user' and 'group'
-     * objects. 
-     *
-     * Web socket headers (generally) don't get passed through SockJS/Shiny Server, so 
-     * this has no effect when running through Shiny Server.
-     */
-    //req.headers['SHINY_SERVER_CREDENTIALS'] = '{"user": "jeff", "groups":["group1", "group2"]}';
-
-    server.proxy.proxyWebSocketRequest(req, socket, head, {host: destHostname, port: destPort});
-});
+if (argv.ssl){
+  https.createServer(options.https, function (req, res) {
+    req.headers['SOME_AUTH'] = 'SOMETHING';
+    proxy.proxyRequest(req, res);
+  }).listen(port);
+} else{
+  http.createServer(function (req, res) {
+    req.headers['SOME_AUTH'] = 'SOMETHING';
+    proxy.proxyRequest(req, res);
+  }).listen(port);
+}
